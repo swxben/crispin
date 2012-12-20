db8c2fb0cca7fd99ae9b4dfb8b7fac6eaf47f2ea
Ben Scott <ben@belfryimages.com.au>
2012-12-20 13:52:23 +1000

    split Apache FOP out into its own nuget package

--- build-nuget-packages.cmd
+++ build-nuget-packages.cmd
@@ -4,6 +4,7 @@ mkdir log\
 mkdir lib\net40\
 
 msbuild.exe /ToolsVersion:4.0 "src\swxben.reporting\swxben.reporting.csproj" /p:configuration=Release
+utilities\nuget.exe pack swxben.fop.nuspec
 utilities\nuget.exe pack swxben.reporting.nuspec
 utilities\nuget.exe pack swxben.reporting.razor.nuspec
 pause
